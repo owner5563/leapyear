c4361925b61e86de192c7534f2ace6109fb90a3d
Kyle Parsons <23gdsm@gmail.com>
2018-06-06 14:31:51 -0700

    add last step for business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,9 @@
 //business logic
 var leapYear = function(year) {
-  if (year % 100 === 0){
-    return false
-  }  else if (year % 4 === 0){
+  if ((year % 4 === 0) && (year % 100 !=== 0) || (year % 400 === 0)){
     return true;
   } else {
     return false;
-
 
   }
 };
@@ -25,7 +22,7 @@ $(document).ready(function() {
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
     $("#result").text(result);
-  console.log(leapYear)
+  console.log()
   });
 
 });
