324ecd0f300fedf7b399831caa3d5aad1b8b4979
Kyle Parsons <23gdsm@gmail.com>
2018-06-06 14:14:03 -0700

    add j script and first business logic function

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+//business logic
+var leapYear = function(year) {
+  return false;
+};
+
+
+
+
+
+
+
+
+//user interface logic
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val())
+    var result = leapYear(year);
+    $("#result").text(result);
+  console.log(leapYear)
+  });
+
+});
